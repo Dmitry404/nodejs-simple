954e3c53c202e7a89dc67b64de91ed977523f9ea
Dmitriy Butakov <dbutakov@lohika.com>
2017-12-16 19:44:00 +0200

    Phase 1 : Implemented minimal app requirements

---
+++ phase1/src/README.md
@@ -0,0 +1,7 @@
+# This is phase1 app 8-)
+
+``` js
+console.log('I am afraid Google Chrome will not render Markdown, darling');
+```
+
+:wink:
---
+++ phase1/src/app.js
@@ -0,0 +1,24 @@
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const morgan = require('morgan');
+const moment = require('moment');
+const express = require('express');
+
+const app = express();
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('<h2>Hello Worm</h2> Go to <a href="http://localhost:3000/api/v1/readme">http://localhost:3000/api/v1/readme</a> to get started');
+});
+router.get('/api/v1/readme', (req, res) => {
+  const options = {
+    root: __dirname,
+    headers: {
+      'Content-Type': 'text/markdown',
+    },
+  };
+  res.sendFile('README.md', options);
+});
+
+app.use('/', router);
+app.listen(3000, () => console.log('Phase 1 app is running on http://localhost:3000'));
