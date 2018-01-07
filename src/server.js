729925876cfee9df8c33b6b6b475f21c63e0eb9d
Dmitriy Butakov <dmitry404@gmail.com>
2018-01-08 00:47:25 +0200

    Code restructuring for phase1 tagging

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
+const winston = require('winston');
+const moment = require('moment');
 const http = require('http');
+const app = require('./app');
 
-http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello Worm!');
-}).listen(3000);
+const hostname = 'localhost';
+const port = 3000;
 
-console.log('Test server is running on http://localhost:3000');
+http.createServer(app).listen(port, () => {
+  winston.info(`Phase 1 app has been started on http://${hostname}:${port} at ${moment().toISOString()}`);
+});
