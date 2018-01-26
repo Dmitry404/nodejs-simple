43a67a78fcbf881955032924c0e02790260b5858
Dmitriy Butakov <dmitry404@gmail.com>
2018-01-26 17:04:22 +0200

    Moving postinstallation step to script / modified installation procedure - copy JWT conf file during installation - use server port from package.json

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const app = require('./app');
 
 const hostname = 'localhost';
-const port = 3000;
+const port = process.env.npm_package_config_server_port || 3000;
 
 http.createServer(app).listen(port, () => {
   winston.info(`Phase 3 app has been started on http://${hostname}:${port} at ${moment().toISOString()}`);
